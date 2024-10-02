ea7438fef40ed16a973298fd6a0a1919abd6a098
Dony667 <73540074+Dony667@users.noreply.github.com>
2024-10-02 22:02:03 +0500

    Add files via upload

---
+++ builder.module06practice.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Computer
+{
+    public string CPU { get; set; }
+    public string RAM { get; set; }
+    public string HDD/SSD { get; set;}
+public string GPU { get; set; }
+public string OS { get; set; }
+private static ToString()
+{
+    Console.Writeline()
+    }
+}
+
+public interface IComputerBuilder
+{
+    void SetCPU();
+    void SetRAM();
+    void SetHDD/SSD();
+    void SetGPU();
+    void SetOS();
+}
+
+public class OfficeComputerBuilder : IComputerBuilder
+{
+    private Computer computer;
+    public void SetCPU()
+    {
+        computer.CPU = "простой процессор";
+    }
+}
+
+public class ComputerDirector
+{
+    public void CreateComputer(IComputerBuilder computerBuilder)
+    {
+        computerBuilder.SetCPU("простой процессор")
+    }
+}
