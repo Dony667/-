9fa27defa57981ac6a44180b031b0ca1f242a9bd
Dony667 <73540074+Dony667@users.noreply.github.com>
2024-10-16 17:19:11 +0500

    Add files via upload

---
+++ Module08LabShablon.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Module08Lab
+{
+    public abstract class Beverage
+    {
+        public void Recept()
+        {
+            Kipitit();
+            Varit();
+            Nalit();
+            Pripravi();
+        }
+
+        private void Kipitit()
+        {
+            Console.WriteLine("Кипячение воды...");
+        }
+
+        private void Nalit()
+        {
+            Console.WriteLine("Наливание в чашку...");
+        }
+
+        protected abstract void Varit();
+        protected abstract void Pripravi();
+    }
+
+    public class Tea : Beverage
+    {
+        protected override void Varit()
+        {
+            Console.WriteLine("Заваривание чая...");
+        }
+
+        protected override void Pripravi()
+        {
+            Console.WriteLine("Добавление лимона...");
+        }
+    }
+
+    public class Coffee : Beverage
+    {
+        protected override void Varit()
+        {
+            Console.WriteLine("Заваривание кофе...");
+        }
+
+        protected override void Pripravi()
+        {
+            Console.WriteLine("Добавление сахара и молока...");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beverage tea = new Tea();
+            Console.WriteLine("Приготовление чая:");
+            tea.Recept();
+
+            Console.WriteLine();
+
+            Beverage coffee = new Coffee();
+            Console.WriteLine("Приготовление кофе:");
+            coffee.Recept();
+        }
+    }
+}
---
+++ module08LabCommand.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Module08Lab
+{   
+    public class Light
+    {
+        public void On()
+        {
+            Console.WriteLine("Свет on");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("Свет off");
+        }
+    }
+
+
+    public interface ICommand
+    {
+        void Execute();
+        void Undo();
+    }
+
+    public class LightOnCommand : ICommand
+    {
+        private Light _light;
+        public LightOnCommand(Light light)
+        {
+            _light = light;
+        }
+
+
+        public void Execute()
+        {
+            _light.On();
+        }
+           
+        public void Undo()
+        {
+            _light.Off();
+        }
+    }
+    public class LightOffCommand : ICommand
+    {
+        private Light _light;
+
+        public LightOffCommand(Light light)
+        {
+            _light = light;
+        }
+
+        public void Execute()
+        {
+            _light.Off();
+        }
+
+        public void Undo()
+        {
+            _light.On();
+        }
+    }
+   
+
+    public class RemoteControl
+    {
+        private ICommand _onCommand;
+        private ICommand _offCommand;
+        public void SetCommands(ICommand onCommand, ICommand offCommand)
+        {
+            _onCommand = onCommand;
+            _offCommand = offCommand;
+        }
+
+        public void PressOnButton()
+        {
+            _onCommand.Execute();
+        }
+
+        public void PressOffButton()
+        {
+            _offCommand.Execute();
+        }
+
+        public void PressUndoButton()
+        {
+            _onCommand.Undo();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Light livingRoomLight = new Light();
+            var lightOn = new LightOnCommand(livingRoomLight);
+            var lightOff = new LightOffCommand(livingRoomLight);
+
+            RemoteControl remote = new RemoteControl();
+            remote.SetCommands(lightOn, lightOff);
+            Console.WriteLine("Управление светом:");
+            remote.PressOnButton();
+            remote.PressOffButton();
+            remote.PressUndoButton();
+
+        }
+    }
+}
