f993b6bdc8bd0e6bd1aae663d4af19070b70e6ed
Dony667 <73540074+Dony667@users.noreply.github.com>
2024-11-02 09:22:51 +0500

    Add files via upload

---
+++ AdapterModule09Practice.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module09Practice
+{
+    internal class Class1
+    {
+        public interface IInternalDeliveryService
+        {
+            void DeliverOrder(string oredId);
+            string GetDeliveryStatus(string oredId);
+        }
+
+        public class InternalDeliveryService : IInternalDeliveryService
+        {
+            public void DeliverOrder(string oredId)
+            {
+                Console.WriteLine("Order DeliverOrder: " + oredId);
+            }
+
+            public string GetDeliveryStatus(string oredId)
+            {
+                return "Satus for Order " + oredId;
+            }
+        }
+        public class GlovoLogisticService
+        {
+            public void ShipItem(int ItemId);
+
+            public string TrackShipment(int ItemId);
+            
+        }
+
+        public class LogisticAdapterGlovo : IInternalDeliveryService
+        {
+            private GlovoLogisticService glovoLogistics;
+            public LogisticAdapterGlovo(GlovoLogisticService glovoLogistics)
+            {
+                this.glovoLogistics = glovoLogistics;
+            }
+            public void DeliverOrder(string oredId)
+            {
+                int item = int.Parse(oredId);
+                glovoLogistics.ShipItem(item);
+            }
+
+            public string GetDeliveryStatus(string orderId)
+            {
+                int item = int.Parse(orderId);
+                return glovoLogistics.TrackShipment(item);
+            }
+        }
+    }
+}
+
+       
+    static void Main(string[] args)
+    {
+     IInternalDeliveryService service = null;
+
+     string typeDelivery = "Glovo"
+         if(typeDelivery == "Glovo")
+        {
+            service = new LogisticAdapterGlovo(new GlovoLogisticsService());
+         }
+         else
+        {
+            service = new IInternalDeliveryService();
+        }
+    service.DeliveryyOrder("1241241");
+        string st = service.GetDeliveryStatus("1241241");
+    }
---
+++ DecoratorModule09Lab.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module08Practice
+{
+    internal class Program
+    {
+        public interface IReport
+        {
+            string Generate();
+        }
+
+        public class SalesReport : IReport
+        {
+            public string Generate()
+            {
+                return "SalesReport";
+            }
+
+        }
+        public class UserReport : IReport
+        {
+            public string Generate()
+            {
+                return "UserReport";
+            }
+
+        }
+
+        public abstract class ReposrtDecorator : IReport
+        {
+            private IReport report;
+
+            protected ReposrtDecorator(IReport report)
+            {
+                this.report = report;
+            }
+            public virtual string Generate()
+            {
+                return report.Generate();
+            }
+        }
+
+        public class DateFilterDecorator : ReposrtDecorator
+        {
+            private DateTime startDate;
+            private DateTime endDate;
+
+            public DateFilterDecorator(IReport report, 
+                DateTime startDate,
+                DateTime endDate) :base(report)
+            {
+                this.startDate = startDate;
+                this.endDate = endDate;
+            }
+
+            public override string Generate()
+            {
+                var data = base.Generate();
+                return "Filter from " + startDate + "" + data;
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            IReport report = new SalesReport();
+            report = new DateFilterDecorator(report, DateTime.Now.AddYears(-1), DateTime.Now);
+            report = new DateFilterDecorator(report, DateTime.Now.AddYears(2), DateTime.Now);
+        }
+    }
+}
